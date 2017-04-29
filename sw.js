5fdc34a6084cc22ba675caf43c3ea592e6cb539e
Vladislav Khvostov <vladkhvo@gmail.com>
2017-04-29 19:48:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,2 @@
 console.log('sw setup');
-this.addEventListener('install', function(event) {
-  console.log(event);
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-      ]);
-    })
-  );
-});
+
